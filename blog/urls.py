144fa9ff20bb4437a9763d749292565b1255f954
Richard-L4 <rwells945@gmail.com>
2025-05-17 20:02:53 +0100

    url comment to blog url file

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-
+# URL configurations for the 'blog' app
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
